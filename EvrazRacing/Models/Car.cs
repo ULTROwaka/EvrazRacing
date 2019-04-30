79daacfc63a52394b9daae23a2abd22ed26dfcc6
ULTROwaka <click.95@mail.ru>
2019-04-30 13:25:27 +0700

    Track model added Replace car field with property

--- EvrazRacing/Models/Car.cs
+++ EvrazRacing/Models/Car.cs
@@ -8,14 +8,62 @@ namespace EvrazRacing.Models
 {
     internal abstract class Car
     {
-        protected string Name;
-        protected float Speed;
-        protected float Passed;
-        protected uint BreakChance;
-        protected bool IsOnPitstop;
-        protected uint OnPitstopTime;
-        protected uint RepairTime;
         protected Random rand;
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            protected set => _name = value;
+        }
+
+        private float _speed;
+        public float Speed
+        {
+            get => _speed;
+            protected set => _speed = value;
+        }
+
+        private float _passed;
+        public float Passed
+        {
+            get => _passed;
+            protected set => _passed = value;
+        }
+
+        private uint _breakChance;
+        public uint BreakChance
+        {
+            get => _breakChance;
+            protected set => _breakChance = value;
+        }
+
+        private bool _isOnPitstop;
+        public bool IsOnPitstop
+        {
+            get => _isOnPitstop;
+            protected set => _isOnPitstop = value;
+        }
+
+        private uint _onPitstopTime;
+        public uint OnPitstopTime
+        {
+            get => _onPitstopTime;
+            protected set => _onPitstopTime = value;
+        }
+
+        private uint _repairTime;
+        public uint RepairTime
+        {
+            get => _repairTime;
+            protected set => _repairTime = value;
+        }
+
+        private bool _isOnFinish;
+        public bool IsOnFinish
+        {
+            get => _isOnFinish;
+            set => _isOnFinish = value;
+        }
 
         protected Car(string name, float speed, uint breakChance, uint repairTime)
         {
---
+++ EvrazRacing/Models/Track.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EvrazRacing.Models
+{
+    class Track
+    {
+        private bool isStarted;
+        private float Distance;
+        private float Delta;
+        private List<Car> CarsOnTrack;
+        private int FinishedCarsCount;
+        Timer RaceTimer;   
+
+        public Track(float distance, int interval)
+        {
+            Distance = distance;
+            Delta = interval / 1000;
+            isStarted = false;
+            CarsOnTrack = new List<Car>();
+            FinishedCarsCount = 0;
+            RaceTimer = new Timer(interval);
+            RaceTimer.Elapsed += new ElapsedEventHandler(RaceTimerTick);
+        }
+
+        private void RaceTimerTick(Object source, ElapsedEventArgs e)
+        {        
+            Update();
+            if (FinishedCarsCount >= CarsOnTrack.Count)
+            {
+                RaceTimer.Enabled = false;
+            }
+        }
+
+        private void Update()
+        {
+            foreach (var car in CarsOnTrack)
+            {
+                if (!car.IsOnFinish)
+                {
+                    car.Update(Delta);
+                    if(car.Passed >= Distance)
+                    {
+                        FinishedCarsCount++;
+                        car.IsOnFinish = true;
+                    }
+                }                         
+            }       
+        }
+
+        public void Start()
+        {
+            if (isStarted) return;
+       
+            isStarted = true;
+            RaceTimer.Enabled = true;
+        }  
+
+        public void AddCar(Car car)
+        {
+            CarsOnTrack.Add(car);
+        } 
+    }
+}
